e3b0baa857cc649a79c07e27a14bb1fc1beba760
SaeedAchiever <saeedleverage101@gmail.com>
2023-02-16 13:05:13 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ *  C program that outputs with a pust function followed by a new line.
+ */
+int main()
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
+}
