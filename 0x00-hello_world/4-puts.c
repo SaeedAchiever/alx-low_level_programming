c616d5db0b19f1f49963aa45384b4b51c6ee4a6a
SaeedAchiever <saeedleverage101@gmail.com>
2023-02-16 13:14:55 -0800

    Hello with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
- *  main , C program that outputs with a pust function followed by a new line.
+ *  main - C program that outputs with a pust function followed by a new line.
  */
+return (0 succes)
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
