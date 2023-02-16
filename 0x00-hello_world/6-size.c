f136ad8615672d089a5296190947548498691c2b
SaeedAchiever <saeedleverage101@gmail.com>
2023-02-16 14:07:07 -0800

    Size of Data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a function that prints the size of various types
+ * Return 0 (success)
+ */
+int main(void)
+{
+char m;
+int n;
+long int o;
+long long int p;
+float q;
+printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeOf (m));
+printf("Size of a int: %lu byte(s)\n" (unsigned long)sizeOf (n));
+printf("Size of a long int: %lu byte(s)\n" (unsigned long)sizeOf (o));
+printf("Size of a long long int: %lu byte(s)\n" (unsigned long)sizeOf (p));
+printf("Size of a float: %lu byte(s)\n" (unsigned long)sizeOf (q));
+return (0)
+}
