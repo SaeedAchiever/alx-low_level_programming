06116eeba5cf6d1a8a17f0723431af336a2dac5b
SaeedAchiever <saeedleverage101@gmail.com>
2023-02-23 12:26:17 -0800

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _isupper - checks is a character is uppercase
- * @c: carrier integer variable
- * Return: 1 if true, 0 if false
+ * _isupper - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0.
  */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
